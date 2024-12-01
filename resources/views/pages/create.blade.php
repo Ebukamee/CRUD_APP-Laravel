a416410dee5bf7fd5e2d0f202c1830a08ef04aa7
Ebuka <chukwuebukanwokike2007@gmail.com>
2024-12-01 23:23:16 +0100

    Dispalying Input Errors

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -4,16 +4,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create</title>
 </head>
-{{-- @vite('resources/css/app.css') --}}
+@vite('resources/css/app.css')
 <body>
     <form action="{{ route('store') }}" method='POST'>
     @csrf
-        <input type="text" id='name' name='name' required>
-        <input type="text" id='name' name='shape' required>
-        <input type="text" id='name' name='imagePath' required>
-        <input type="text" id='name' name='size' required>
-        <input type="text" id='name' name='dojo_id' required>
+        <input type="text" id='name' name='name' value="{{ old('name') }}" required>
+        <input type="text" id='name' name='shape' value="{{ old('shape') }}" required>
+        <input type="text" id='name' name='imagePath' value="{{ old('imagePath') }}" required>
+        <input type="text" id='name' name='size' value="{{ old('size') }}" required>
+        <input type="text" id='name' name='dojo_id' value="{{ old('dojo_id') }}" required>
         <button>Submit</button>
+
+        @if($errors->any())
+            <ul class="px-4 py-2 bg-red-100">
+              @foreach($errors->all() as $error)
+                <li class="my-2 text-red-500">
+                  {{ $error }}
+                </li>
+              @endforeach
+            </ul>
+        @endif
     </form>
 </body>
 </html>
