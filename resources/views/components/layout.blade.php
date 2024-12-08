6cab29dd01eda8c2654fd98ef30b324d1f127ea0
Ebuka <chukwuebukanwokike2007@gmail.com>
2024-12-08 01:08:15 +0100

    Design Changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,24 +4,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @vite('resources/css/app.css')
+    
 </head>
 <body>
 
-    <nav class="grid grid-cols-6 gap-4 m-10 p-10 py-5 my-5 justify-between">
-         <img src="/logo.png" alt="" class="w-20 col-span-1">
-         <div class='flex  text-xl col-span-3 font-medium justify-between'>
+    <nav class="lg:grid lg:grid-cols-6 gap-4 m-10 p-10 py-5 my-5 justify-between flex">
+        <div>
+          <img src="/logo.png" alt="" class="w-20 col-span-1">
+          <h2 class="text-xl font-semibold logo mt-2">HealthRadar</h2>
+        </div>
+      
+        <div class='lg:flex  text-xl col-span-3 font-medium justify-between hidden'>
             <p class='m-2'>Home</p>
             <p class='m-2'>Hospitals</p>
             <p class='m-2'>Medications</p>
             <p class='m-2'>Create +</p>
 
-</div>
-<div class="col-span-2 text-black ">
-<button class="rounded-3xl p-2 border border-[2px] border-[#46e0d3] px-10 mx-5">Log In</button>
-   <button class="bg-[#46e0d3]  rounded-3xl p-2 px-10 border border-[#46e0d3] mx-5">Sign Up</button>
-</div>
-    </nav>
+        </div>
+        <div class="col-span-2 text-black  ">
+            <button class="rounded-3xl p-2 border border-[2px] border-[#46e0d3] px-10 lg:mx-5 hover">Log In</button>
+            <button class="bg-[#46e0d3]  rounded-3xl p-2 px-10 border border-[#46e0d3] lg:mx-5 hover">Sign Up</button>
+        </div>
 
+        <button id="menu-toggle" class="text-gray-600 focus:outline-none lg:hidden">
+                <svg id="hamburger-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+                </svg>
+                <svg id="close-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 hidden">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        
+    </nav>
+<div id="mobile-menu" class="hidden bg-[#888b95]  lg:hidden p-10 text-white">
+            <p class='m-2'>Home</p>
+            <p class='m-2'>Hospitals</p>
+            <p class='m-2'>Medications</p>
+            <p class='m-2'>Create +</p>
+
+        </div>
     {{$slot}}
+<footer>
+  lol
+</footer>
+     <script>
+        const menuToggle = document.getElementById('menu-toggle');
+        const mobileMenu = document.getElementById('mobile-menu');
+        const hamburgerIcon = document.getElementById('hamburger-icon');
+        const closeIcon = document.getElementById('close-icon');
+
+        menuToggle.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+            hamburgerIcon.classList.toggle('hidden');
+            closeIcon.classList.toggle('hidden');
+        });
+    </script>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,75 +7,75 @@
 @vite('resources/css/app.css')
 <body>
     <x-hero>
-  <div class="mt-10">
-    <h1 class="text-bold text-3xl text-center">Designed For the Future</h1>
-    <div class="flex m-5 p-5 justify-between">
-        <div class="m-10 p-2 p">
-            <div class="m-2 p-2">
-                <h2 class="text-xl text-700">Introducing The Best Editor</h2>
-                <p class="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus quibusdam tenetur molestiae maxime autem et reprehenderit, illum facere quas sapiente placeat eligendi nostrum ea amet recusandae unde ut earum.</p>
+        <div class="mt-10">
+            <h1 class="text-bold text-3xl text-center">Designed For the Future</h1>
+            <div class="flex m-5 p-5 justify-between">
+                <div class="m-10 p-2 p">
+                    <div class="m-2 p-2">
+                        <h2 class="text-xl text-700">Introducing The Best Editor</h2>
+                        <p class="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus quibusdam tenetur molestiae maxime autem et reprehenderit, illum facere quas sapiente placeat eligendi nostrum ea amet recusandae unde ut earum.</p>
+                    </div>
+                    <div class="m-2 p-2">
+                        <h2 class="text-xl text-bold">Robust Content Management</h2>
+                        <p class="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus quibusdam tenetur molestiae maxime autem et reprehenderit, illum facere quas sapiente placeat eligendi nostrum ea amet recusandae unde ut earum.</p>
+                    </div>
+                </div>
+                <img src="illustration-editor-desktop.svg" class="w-1/2" alt="">
+
             </div>
-            <div class="m-2 p-2">
-                <h2 class="text-xl text-bold">Robust Content Management</h2>
-                <p class="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus quibusdam tenetur molestiae maxime autem et reprehenderit, illum facere quas sapiente placeat eligendi nostrum ea amet recusandae unde ut earum.</p>
+            <div class="b rounded-tr-3xl rounded-bl-3xl p-5 flex justify-between max-h-[400px]">
+                <img src="illustration-phones.svg" alt="" class="h-full relative bottom-20">
+                <div class="m-2 p-2">
+                    <h2 class="text-2xl text-white my-5">
+                        State of the Art Infratructure
+                    </h2>
+                    <p class="lg text-[hsl(240,2%,79%)]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nulla, aliquid tempore incidunt facere culpa maxime suscipit! Ipsum cum, quae iusto perspiciatis nostrum quidem est fugit autem unde corporis voluptatum.
+                    </p>
+                </div>
+            </div>
+            <div class="m mt-48  flex justify-between">
+                <img src="illustration-laptop-desktop.svg" alt="" class="w-1/2 relative right-48 bottom-5">
+                <div class="m-5 p-2 relative right-20 top-10">
+                    <div class="m-2 p-2">
+                        <h2 class="text-xl text-700 my-5">Free, open, simple</h2>
+                        <p class="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus quibusdam tenetur molestiae maxime autem et reprehenderit, illum facere quas sapiente placeat eligendi nostrum ea amet recusandae unde ut earum.</p>
+                    </div>
+                    <div class="m-2 p-2">
+                        <h2 class="text-xl text-bold my-5">Powerful tooling</h2>
+                        <p class="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus quibusdam tenetur molestiae maxime autem et reprehenderit, illum facere quas sapiente placeat eligendi nostrum ea amet recusandae unde ut earum.</p>
+                    </div>
+                </div>
+
             </div>
         </div>
-        <img src="illustration-editor-desktop.svg" class="w-1/2" alt="">
-
-  </div>
-  <div class="b rounded-tr-3xl rounded-bl-3xl p-5 flex justify-between max-h-[400px]">
-    <img src="illustration-phones.svg" alt="" class="h-full relative bottom-20">
-    <div class="m-2 p-2">
-        <h2 class="text-2xl text-white my-5">
-            State of the Art Infratructure
-        </h2>
-        <p class="lg text-[hsl(240,2%,79%)]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nulla, aliquid tempore incidunt facere culpa maxime suscipit! Ipsum cum, quae iusto perspiciatis nostrum quidem est fugit autem unde corporis voluptatum.
-        </p>
-    </div>
-  </div>
-  <div class="m mt-48  flex justify-between">
-    <img src="illustration-laptop-desktop.svg" alt="" class="w-1/2 relative right-48 bottom-5">
-    <div class="m-5 p-2 relative right-20 top-10">
-            <div class="m-2 p-2">
-                <h2 class="text-xl text-700 my-5">Free, open, simple</h2>
-                <p class="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus quibusdam tenetur molestiae maxime autem et reprehenderit, illum facere quas sapiente placeat eligendi nostrum ea amet recusandae unde ut earum.</p>
+        <div class="footer rounded-tr-[100px] p-10 px-20 flex justify-between">
+            <div>
+                <img src="logo.svg" alt="">
             </div>
-            <div class="m-2 p-2">
-                <h2 class="text-xl text-bold my-5">Powerful tooling</h2>
-                <p class="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellendus quibusdam tenetur molestiae maxime autem et reprehenderit, illum facere quas sapiente placeat eligendi nostrum ea amet recusandae unde ut earum.</p>
+            <div>
+                <p class="text-600 mb-10 text-white">Product</p>
+                <p class="text-[hsl(240,2%,79%)]">Overview</p>
+                <p class="text-[hsl(240,2%,79%)]">Pricing</p>
+                <p class="text-[hsl(240,2%,79%)]">Marketplace</p>
+                <p class="text-[hsl(240,2%,79%)]">Features</p>
+                <p class="text-[hsl(240,2%,79%)]">Integrations</p>
+            </div>
+            <div>
+                <p class="text-bold mb-10 text-white">Company</p>
+                <p class="text-[hsl(240,2%,79%)]">About</p>
+                <p class="text-[hsl(240,2%,79%)]">Team</p>
+                <p class="text-[hsl(240,2%,79%)]">Blog</p>
+                <p class="text-[hsl(240,2%,79%)]">Careers</p>
+            </div>
+            <div>
+                <p class="text-bold mb-10 text-white">Connect</p>
+                <p class="text-[hsl(240,2%,79%)]">Contact</p>
+                <p class="text-[hsl(240,2%,79%)]">Newsletter</p>
+                <p class="text-[hsl(240,2%,79%)]">LinkedIn</p>
             </div>
         </div>
-
-  </div>
-  </div>
-  <div class="footer rounded-tr-[100px] p-10 px-20 flex justify-between">
-    <div>
-    <img src="logo.svg" alt="">
-    </div>
-    <div>
-        <p class="text-600 mb-10 text-white">Product</p>
-        <p class="text-[hsl(240,2%,79%)]">Overview</p>
-        <p  class="text-[hsl(240,2%,79%)]">Pricing</p>
-        <p  class="text-[hsl(240,2%,79%)]">Marketplace</p>
-        <p  class="text-[hsl(240,2%,79%)]">Features</p>
-        <p  class="text-[hsl(240,2%,79%)]">Integrations</p>
-    </div>
-<div>
-        <p class="text-bold mb-10 text-white">Company</p>
-        <p  class="text-[hsl(240,2%,79%)]">About</p>
-        <p  class="text-[hsl(240,2%,79%)]">Team</p>
-        <p class="text-[hsl(240,2%,79%)]">Blog</p>
-        <p class="text-[hsl(240,2%,79%)]">Careers</p>
-    </div>
-    <div>
-        <p class="text-bold mb-10 text-white">Connect</p>
-        <p  class="text-[hsl(240,2%,79%)]">Contact</p>
-        <p  class="text-[hsl(240,2%,79%)]">Newsletter</p>
-        <p  class="text-[hsl(240,2%,79%)]">LinkedIn</p>
-    </div>
-</div>
-</x-hero>
+    </x-hero>
 </body>
 </html> --}}
 
@@ -84,20 +84,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HealthRadar</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css">
     @vite('resources/css/app.css')
+    
 </head>
 
 <body>
-    
+
     <x-layout>
-      <div class="bg-[#e5f1fe] m-5 mx-10 pt-10 rounded-3xl text-center h-fit bg-cover bg-no-repeat" style="background-image: url('background.png');">
-       <h2 class="text-6xl font-semibold text-center">Everyone deserves quality healthcare.</h2>
-       <p class="text-center text-xl m-5">With HealthRadar, View Healthcare centers around you and browse through various medications. You can also Join us to create a comprehensive list and bring quality healthcare closer to home</p>
-        <button class="bg-[#46e0d3]  rounded-3xl p-2 px-10 border border-[#46e0d3] mx-5 m-10">Join Us</button>
-        <div class="w-full h-fit">
-          <img src="man.png" alt="" class="max-w-[600px] relative left-[600px]">
+        <div class="bg-[#e5f1fe] m-5 mx-10 pt-10 rounded-3xl text-center h-fit bg-cover bg-no-repeat" style="background-image: url('background.png');">
+            <h2 class="text-6xl font-semibold text-center" data-aos="flip-left">Everyone deserves quality healthcare.</h2>
+            <p class="text-center text-xl m-5" data-aos="fade-up">With HealthRadar, View Healthcare centers around you and browse through various medications. You can also Join us to create a comprehensive list and bring quality healthcare closer to home</p>
+            <button class="teal">Join Us</button>
+            <div class="w-full h-fit grid justify-items-end">
+                <img src="man.png" alt="" class="max-w-[600px]" data-aos="zoom-in">
+            </div>
         </div>
-      </div>
+        <div class="m-10 p-10">
+            <h2 class="text-6xl font-medium mb-10" data-aos="fade-up">Who we serve?</h2>
+            <div class="grid gap-4 grid-cols-3">
+                <p class="text-lg  col-span-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, cum modi tempore vitae maxime expedita voluptate alias animi! Distinctio unde ipsam ut delectus dolorum. Velit incidunt repellendus dignissimos tenetur natus.</p>
+                <button class="teal">See all our Services</button>
+            </div>
+            <div class="grid grid-cols-6 gap-4 ">
+                <div class="bg-no-repeat bg-cover p-10 bg-[#e5f1fe] rounded-2xl col-span-4 " style="background-image:url('wws.svg');">
+                    <div>
+                        <img src="doctor.svg" alt="">
+                    </div>
+                    <h2 class="my-5 text-4xl font-semibold">Our Providers</h2>
+                    <p class="text-xl w-[70%] mb-[100px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, sint ab. Temporibus nemo aspernatur officia odit fuga nihil explicabo sapiente voluptas, placeat molestiae dolores animi voluptatem impedit dignissimos quod quos?</p>
+                    <button class="teal">Solutions</button>
+                </div>
+                <div class="grid grid-cols-3 gap-4 col-span-2">
+                    <div class="rounded-2xl border py-10 col-span-1">
+                        <img src="people.svg" alt="" class="m-auto block w-20 h-20">
+                        <h2 class="text-2xl transform rotate-90 font-semibold mt-24">People</h2>
+                    </div>
+                    <div class="rounded-2xl border py-10 col-span-1">
+                        <img src="s.svg" alt="" class="m-auto block w-20 h-20">
+                        <h2 class="text-2xl transform rotate-90 font-semibold mt-24">Society</h2>
+                    </div>
+                    <div class="rounded-2xl border py-10 col-span-1">
+                        <img src="bs.svg" alt="" class="m-auto block w-20 h-20">
+                        <h2 class="text-2xl transform rotate-90 font-semibold mt-24">Consultants</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
     </x-layout>
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            duration: 2000
+            , once: true
+        , });
+
+    </script>
 </body>
 </html>
