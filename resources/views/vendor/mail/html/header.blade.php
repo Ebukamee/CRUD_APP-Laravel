8ab06bbcfd8b2345cd4b593a7aec29403923dc53
Ebuka <chukwuebukanwokike2007@gmail.com>
2024-12-21 23:43:44 +0100

    Email Template Changes

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@if (trim($slot) === 'HealthRadar')
+<img src="https://firebasestorage.googleapis.com/v0/b/giftcard-9951a.appspot.com/o/products%2Flogo.png?alt=media&token=6e972c98-dffb-4520-ac9d-6546dcf1d521" alt="Health Radar Logo">
 @else
 {{ $slot }}
 @endif
--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -2,11 +2,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>{{ config('app.name') }}</title>
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="color-scheme" content="light">
 <meta name="supported-color-schemes" content="light">
 <style>
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;1,9..40,300&family=League+Spartan:wght@300;500;700&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Poppins:wght@600&family=Quicksand:wght@300;400;500;600&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+.inner-body {
+    font-family: "DM Sans", sans-serif;
+}
 @media only screen and (max-width: 600px) {
 .inner-body {
 width: 100% !important;
@@ -21,6 +26,26 @@ width: 100% !important;
 .button {
 width: 100% !important;
 }
+}
+img {
+    width:70px;
+    height:50px;
+}
+
+.button {
+  border: 1px solid #46e0d3;
+  background-color: #46e0d3;
+  border-radius: 9999px; /* Equivalent to rounded-3xl */
+  padding: 0.5rem 2rem; /* p-2 px-8 */
+  margin: 2.5rem 0; /* mx-0 m-10 */
+  font-weight: 500; /* font-medium */
+  transition: all 0.3s ease; /* Default transition for smooth hover effect */
+}
+
+.button:hover {
+  border: 2px solid #002265; /* hover:border-[#002265] with [2px] border */
+  background-color: white; /* hover:bg-white */
+  color: #0d41c0; /* hover:text-[#0d41c0] */
 }
 </style>
 {{ $head ?? '' }}
